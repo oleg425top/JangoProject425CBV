df1b9f2395149b1a2093755522135eaf8d85a23b
oleg425top <oleg59152@gmail.com>
2025-03-28 13:18:25 +0500

    создали миграции!

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
+
 
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # project apps
+
 ]
 
 MIDDLEWARE = [
@@ -75,10 +82,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+USER = os.getenv('MS_SQL_USER')
+PASSWORD = os.getenv('MS_SQL_KEY')
+HOST = os.getenv('MS_SQL_SERVER')
+DATABASE = os.getenv('MS_SQL_DATABASE')
+# БАза данных для создания другой
+PAD_DATABASE = os.getenv('MS_PAD_DATABASE')
+DRIVER = os.getenv('MS_SQL_DRIVER')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': DATABASE,
+        'HOST':HOST,
+        'PORT':'',
+        'OPTIONS':{
+            'driver': DRIVER
+        }
     }
 }
 
