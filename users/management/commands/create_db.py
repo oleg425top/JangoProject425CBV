60e417e9ab4f3af53cd659b864f70a04b09d91b5
oleg425top <oleg59152@gmail.com>
2025-03-28 15:06:59 +0500

    1: Добавлена команда создания Базы Данных

---
+++ users/management/commands/create_db.py
@@ -0,0 +1,22 @@
+from django.core.management import BaseCommand
+import pyodbc
+from config.settings import USER, PASSWORD, HOST, DRIVER, PAD_DATABASE, DATABASE
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        ConnectionString = f'''DRIVER={DRIVER};
+                        SERVER={HOST};
+                        DATABASE={PAD_DATABASE};
+                        UID={USER};
+                        PWD={PASSWORD};'''
+
+        try:
+            conn = pyodbc.connect(ConnectionString)
+            conn.autocommit = True
+            # conn.execute(fr'DROP DATABASE {DATABASE}')
+            conn.execute(fr'CREATE DATABASE {DATABASE}')
+        except pyodbc.ProgrammingError as ex:
+            print(ex)
+        else:
+            print(f'Database {DATABASE} was created successfully!!')
