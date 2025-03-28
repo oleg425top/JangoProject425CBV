9845a45c3892444b3fdc6e32d6b889ae14336570
oleg425top <oleg59152@gmail.com>
2025-03-28 14:26:00 +0500

    1: Добавлена настройка для авторизации пользователя

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 
-
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -20,7 +19,6 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -32,7 +30,6 @@ SECRET_KEY = 'django-insecure-ckw3-kzr@!^cu%9%2kf32+weu6#y7k=fpu=7z&1@#i98_^*(yv
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -78,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -94,14 +90,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'mssql',
         'NAME': DATABASE,
-        'HOST':HOST,
-        'PORT':'',
-        'OPTIONS':{
+        'HOST': HOST,
+        'PORT': '',
+        'OPTIONS': {
             'driver': DRIVER
         }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -121,7 +116,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -133,7 +127,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -143,3 +136,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
