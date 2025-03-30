ffb61afc9f3c1406fd253cdd02344e946e68cc4a
oleg425top <oleg59152@gmail.com>
2025-03-30 16:55:18 +0500

    Прописаны настройки static и media

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ import os
 
 from pathlib import Path
 
+from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -132,6 +133,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    BASE_DIR/ 'static',
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (
+    BASE_DIR / 'media'
+)
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
