360a56bb985c7bea0ca8bbdd13cdb693daa1e6fe
oleg425top <oleg59152@gmail.com>
2025-04-13 14:13:26 +0500

    Реализованы сервисы: 1 Уведомление о регистрации 2 Отправка нового пароля

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from users.apps import UsersConfig
 
 from users.views import user_register_view, user_profile_view, user_login_view, user_logout_view, user_update_view, \
-    user_change_password_view
+    user_change_password_view, user_generate_new_password_view
 
 app_name = UsersConfig.name
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path('profile/', user_profile_view, name='user_profile'),
     path('update/', user_update_view, name='user_update'),
     path('change_password/', user_change_password_view, name='user_change_password'),
+    path('profile/genpassword', user_generate_new_password_view, name='user_generate_new_password'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,14 @@
+import random
+import string
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, reverse, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib import messages
 
-from users.forms import UserRegisterForm, UserLoginForm, UserForm, UserUpdateForm, UserChangePasswordForm
+from users.forms import UserRegisterForm, UserLoginForm, UserUpdateForm, UserChangePasswordForm
+from users.services import send_register_email, send_new_password
 
 
 def user_register_view(request):
@@ -13,10 +17,9 @@ def user_register_view(request):
 
         if form.is_valid():
             new_user = form.save()
-            print(new_user)
-            print(form.cleaned_data['password'])
             new_user.set_password(form.cleaned_data['password'])
             new_user.save()
+            send_register_email(new_user.email)
             return HttpResponseRedirect(reverse('users:user_login'))
     context = {
         'title': 'Создать аккаунт',
@@ -72,6 +75,7 @@ def user_update_view(request):
     }
     return render(request, "users/user_update.html", context=context)
 
+
 @login_required(login_url='users:user_login')
 def user_change_password_view(request):
     user_object = request.user
@@ -95,3 +99,12 @@ def user_change_password_view(request):
 def user_logout_view(request):
     logout(request)
     return redirect('dogs:index')
+
+
+@login_required(login_url='users:user_login')
+def user_generate_new_password_view(request):
+    new_password = ''.join(random.sample((string.ascii_letters + string.digits), 12))
+    request.user.set_password(new_password)
+    request.user.save()
+    send_new_password(request.user.email, new_password)
+    return redirect(reverse('dogs:index'))
