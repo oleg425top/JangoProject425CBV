c3b9f890670b761f558d73022f97f0ee5db9fdf4
oleg425top <oleg59152@gmail.com>
2025-04-21 17:00:42 +0500

    Переписали dogs_create_view >> DogCreateView Внесли изменения в dogs.urls

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,8 +1,7 @@
 from  django.urls import path
 from dogs.apps import DogsConfig
 
-from dogs.views import index_view, breeds_list_view, breed_dogs_list_view, dog_create_view, \
-    dog_detail_view, dog_update_view, dog_delete_view, DogListView
+from dogs.views import index_view, breeds_list_view, breed_dogs_list_view, dog_detail_view, dog_update_view, dog_delete_view, DogListView, DogCreateView
 
 app_name = DogsConfig.name
 
@@ -14,7 +13,7 @@ urlpatterns = [
     #dogs
     path('breeds/<int:pk>/dogs/',breed_dogs_list_view, name='breed_dogs'),
     path('dogs', DogListView.as_view(), name='dogs_list'),
-    path('dogs/create/', dog_create_view, name='dog_create'),
+    path('dogs/create/', DogCreateView.as_view(), name='dog_create'),
     path('dogs/detail/<int:pk>/', dog_detail_view, name='dog_detail'),
     path('dogs/update/<int:pk>/', dog_update_view, name='dog_update'),
     path('dogs/delete/<int:pk>/', dog_delete_view, name='dog_delete'),
--- dogs/views.py
+++ dogs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
@@ -10,7 +10,7 @@ from dogs.forms import DogForms
 
 def index_view(request):
     context = {
-        'objects_list': Breed.objects.all()[:3],
+        'object_list': Breed.objects.all()[:3],
         'title': 'Питомник - Главная',
     }
 
@@ -19,7 +19,7 @@ def index_view(request):
 
 def breeds_list_view(request):
     context = {
-        'objects_list': Breed.objects.all(),
+        'object_list': Breed.objects.all(),
         'title': 'Питомник - Все наши породы',
     }
     return render(request, 'dogs/breeds.html', context=context)
@@ -28,7 +28,7 @@ def breeds_list_view(request):
 def breed_dogs_list_view(request, pk: int):
     breed_object = Breed.objects.get(pk=pk)
     context = {
-        'objects_list': Dog.objects.filter(breed_id=pk),
+        'object_list': Dog.objects.filter(breed_id=pk),
         'title': f'Собаки породы - {breed_object.name}',
         'breed_pk': breed_object.pk,
     }
@@ -44,21 +44,15 @@ class DogListView(ListView):
 
 
 # Create Read Update Delete (CRUD)
-@login_required(login_url='users:user_login')
-def dog_create_view(request):
-    if request.method == 'POST':
-        form = DogForms(request.POST, request.FILES)
-        if form.is_valid():
-            dog_object = form.save()
-            dog_object.owner = request.user
-            dog_object.save()
-            return HttpResponseRedirect(reverse('dogs:dogs_list'))
-    context = {
-        'title': 'Добавить собаку',
-        'form': DogForms,
+
+class DogCreateView(CreateView):
+    model = Dog
+    form_class = DogForms
+    template_name = 'dogs/create_update.html'
+    extra_context = {
+        'title': 'Добавить собаку'
     }
-    return render(request, 'dogs/create_update.html', context=context)
-    # return render(request, 'dogs/create.html', {'form': DogForms})   пример без context
+    success_url = reverse_lazy('dogs:dogs_list')
 
 
 @login_required(login_url='users:user_login')
