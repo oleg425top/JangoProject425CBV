7193ec1600c82a0acdaf959c79cba2fbfc28a80f
oleg425top <oleg59152@gmail.com>
2025-05-07 16:30:46 +0500

    Добавлены ReviewDeactivatedListView, ReviewListView Добавлены url's Добавлены шаблоны

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,8 +3,12 @@ from django.urls import path
 from dogs.urls import app_name, urlpatterns
 from reviews.apps import ReviewsConfig
 
+from reviews.views import ReviewListView, ReviewDeactivatedListView, ReviewDeactivatedListView
+
 app_name = ReviewsConfig.name
 
 urlpatterns = [
+    path('', ReviewListView.as_view(), name='reviews_list'),
+    path('deactivated/', ReviewDeactivatedListView.as_view(), name='reviews_deactivated_list'),
 
 ]
